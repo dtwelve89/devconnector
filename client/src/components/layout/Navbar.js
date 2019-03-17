48d8039db8c0547254ba0eefaa21e74268473b8a
Danny Ocean <danny.danh.nguyen@gmail.com>
2019-03-16 19:08:09 -0700

    TextFieldGroupInput component

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -16,7 +16,7 @@ class Navbar extends Component {
     const authLinks = (
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
-          <a href="" onClick={this.onLogoutClick} className="nav-link">
+          <button onClick={this.onLogoutClick} className="nav-link bg-dark">
             <img
               className="rounded-circle"
               src={user.avatar}
@@ -25,7 +25,7 @@ class Navbar extends Component {
               title="You must have a Gravatar connected to your email to display an image"
             />{" "}
             Logout
-          </a>
+          </button>
         </li>
       </ul>
     );
