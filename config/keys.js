ae089c1d66af0a3768c91d79b5e78b5c1f655fd6
Danny Ocean <danny.danh.nguyen@gmail.com>
2019-03-22 12:19:52 -0700

    securing keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://danny:danny12@ds135522.mlab.com:35522/devconnector",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
